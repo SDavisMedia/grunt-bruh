af986f7474d6615b8848391c7f8b06f6996b618c
Sean Davis <sdavis2702@gmail.com>
2018-04-04 12:30:51 -0700

    initial commit

---
+++ assets/js/jsfile1.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+	var issa_var = $('.element');
+});
---
+++ gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+	// Load multiple grunt tasks using globbing patterns
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration.
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+			},
+			target: {
+				files: {
+					'style.css': [ 'assets/css/*.css', '!assets/css/*.min.css', '!assets/css/*jquery*.css' ]
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				mangle: false,
+			},
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'assets/js',
+					src: [ '*.js', '!*.min.js', '!*jquery*.js' ],
+					dest: 'assets/js',
+					ext: '.min.js',
+					extDot: 'last',
+				}]
+			}
+		},
+
+		// Clean up build directory
+		clean: {
+			main: ['build/<%= pkg.name %>']
+		},
+
+		// Copy the plugin into the build directory
+		copy: {
+			main: {
+				src:  [
+					'assets/**',
+					'*.php',
+					'*.css',
+					'*.js',
+					'!gruntfile.js',
+				],
+				dest: 'build/<%= pkg.name %>/'
+			}
+		},
+
+		// Compress build directory into <name>.zip and <name>-<version>.zip
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: './build/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				cwd: 'build/<%= pkg.name %>/',
+				src: ['**/*'],
+				dest: '<%= pkg.name %>/'
+			}
+		},
+
+	});
+
+	// Build task(s).
+	grunt.registerTask( 'build', [ 'cssmin', 'uglify', 'clean', 'copy', 'compress' ] );
+
+};
